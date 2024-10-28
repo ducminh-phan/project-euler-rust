a65b41928277fec2b4c34fa84d858c90e2b004e6
Duc-Minh Phan <alephvn@gmail.com>
2024-10-28 21:44:16 +0700

    feat: Use macros to declare problems collections

--- src/lib/problems/h00/t1/mod.rs
+++ src/lib/problems/h00/t1/mod.rs
@@ -1,27 +1,14 @@
-use std::collections::HashMap;
+use crate::declare_problems;
 
-pub fn get_map() -> HashMap<u32, fn()> {
-    HashMap::from([
-        (0, p0010::solve as fn()),
-        (1, p0011::solve),
-        (2, p0012::solve),
-        (3, p0013::solve),
-        (4, p0014::solve),
-        (5, p0015::solve),
-        (6, p0016::solve),
-        (7, p0017::solve),
-        (8, p0018::solve),
-        (9, p0019::solve),
-    ])
+declare_problems! {
+    p0010,
+    p0011,
+    p0012,
+    p0013,
+    p0014,
+    p0015,
+    p0016,
+    p0017,
+    p0018,
+    p0019,
 }
-
-mod p0010;
-mod p0011;
-mod p0012;
-mod p0013;
-mod p0014;
-mod p0015;
-mod p0016;
-mod p0017;
-mod p0018;
-mod p0019;
