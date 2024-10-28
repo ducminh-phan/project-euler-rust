3eca9d7780a790d8029281ec7a1aa82bcb063983
Duc-Minh Phan <alephvn@gmail.com>
2024-10-28 08:39:33 +0700

    refactor: Update module name

--- src/lib/problems/h00/t1/mod.rs
+++ src/lib/problems/h00/t1/mod.rs
@@ -2,26 +2,26 @@ use std::collections::HashMap;
 
 pub fn get_map() -> HashMap<u32, fn()> {
     HashMap::from([
-        (0, p0::main as fn()),
-        (1, p1::main),
-        (2, p2::main),
-        (3, p3::main),
-        (4, p4::main),
-        (5, p5::main),
-        (6, p6::main),
-        (7, p7::main),
-        (8, p8::main),
-        (9, p9::main),
+        (0, p0010::main as fn()),
+        (1, p0011::main),
+        (2, p0012::main),
+        (3, p0013::main),
+        (4, p0014::main),
+        (5, p0015::main),
+        (6, p0016::main),
+        (7, p0017::main),
+        (8, p0018::main),
+        (9, p0019::main),
     ])
 }
 
-mod p0;
-mod p1;
-mod p2;
-mod p3;
-mod p4;
-mod p5;
-mod p6;
-mod p7;
-mod p8;
-mod p9;
+mod p0010;
+mod p0011;
+mod p0012;
+mod p0013;
+mod p0014;
+mod p0015;
+mod p0016;
+mod p0017;
+mod p0018;
+mod p0019;
