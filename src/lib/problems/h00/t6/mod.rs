c2fe0dd2712455f0011e50458d7c712d97a870f3
Duc-Minh Phan <alephvn@gmail.com>
2024-11-14 20:51:36 +0700

    feat: Problem 63

--- src/lib/problems/h00/t6/mod.rs
+++ src/lib/problems/h00/t6/mod.rs
@@ -4,4 +4,5 @@ declare_problems! {
     p0060,
     p0061,
     p0062,
+    p0063,
 }
---
+++ src/lib/problems/h00/t6/p0063.rs
@@ -0,0 +1,22 @@
+//! The `5`-digit number, `16807=7^5`, is also a fifth power. Similarly, the
+//! `9`-digit number, `134217728=8^9`, is a ninth power.
+//!
+//! How many `n`-digit positive integers exist which are also an `n`th power?
+
+pub fn solve() {
+    // k is an n-th power
+    // => k = b^n >= 1^n
+    // k is an n-digit positive integers
+    // => 10^n > k >= 10^(n-1)
+    // => 1 <= b < 10 and b^n >= 10^(n-1)
+    // => 1 <= b < 10 and n * log10(b) >= n-1
+
+    let result = (1u32..10)
+        .flat_map(|b| {
+            (1u32..).find(|&n| (n as f64) * (b as f64).log10() < (n - 1) as f64)
+        })
+        .map(|n| n - 1)
+        .sum::<u32>();
+
+    dbg!(result);
+}
